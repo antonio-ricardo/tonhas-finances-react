e77305b0ae59d2663644d9ae31856ba8b385ae22
antonio ricardo <antonio.gugou@gmail.com>
2022-12-13 16:44:04 -0300

    feat: implementing mirage.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
@@ -5,6 +6,67 @@ import { App } from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: 'show',
+        value: 123,
+        category: 'show',
+        date: new Date(),
+        type: 'DEPOSIT'
+      }, {
+        id: 2,
+        title: 'show',
+        value: 123,
+        category: 'show',
+        date: new Date(),
+        type: 'DEPOSIT'
+      }, {
+        id: 3,
+        title: 'show',
+        value: 123,
+        category: 'show',
+        date: new Date(),
+        type: 'DEPOSIT'
+      }, {
+        id: 4,
+        title: 'show',
+        value: 123,
+        category: 'show',
+        date: new Date(),
+        type: 'DEPOSIT'
+      }, {
+        id: 5,
+        title: 'show',
+        value: 123,
+        category: 'show',
+        date: new Date(),
+        type: 'DEPOSIT'
+      }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', (schema, request) => {
+
+      const transactions = schema.db.transactions
+
+      return transactions.length > 0 ? transactions : null
+    })
+  }
+})
+
 root.render(
   <React.StrictMode>
     <App />
