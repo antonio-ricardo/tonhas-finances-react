8db88cc34ba4d09c222f887cd0d6d528c1d8077e
antonio ricardo <antonio.gugou@gmail.com>
2022-12-23 15:20:46 -0300

    feat: implemanting options button and logOut option

--- src/components/PrincipalHeader/index.tsx
+++ src/components/PrincipalHeader/index.tsx
@@ -1,6 +1,10 @@
 import logo from '../../assets/logo.png'
+import config from '../../assets/config.png'
+
 import { useUser } from '../../hooks/useUser'
 import { Container, Content } from './styles'
+import { useState } from 'react'
+import { useAuthenticate } from '../../hooks/useAuthenticate'
 
 
 interface Props {
@@ -9,12 +13,18 @@ interface Props {
 
 export function PrincipalHeader({ onOpenNewTransactionModal }: Props) {
     const { user } = useUser()
+    const { logOut } = useAuthenticate()
+    const [isOpenOptions, setIsOpenOptions] = useState<boolean>(false)
 
     const hasProfilePhoto = !!user.photoUrl
 
+    function handleCloseOrOpenOptions() {
+        setIsOpenOptions(!isOpenOptions)
+    }
+
     return (
         <Container>
-            <Content hasProfilePhoto={hasProfilePhoto}>
+            <Content hasProfilePhoto={hasProfilePhoto} isOpenOptions={isOpenOptions}>
                 <div>
                     <img src={logo} alt="tonhas finances" />
                     <strong>Tonhas finance$</strong>
@@ -31,6 +41,14 @@ export function PrincipalHeader({ onOpenNewTransactionModal }: Props) {
                 <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova Transação
                 </button>
+
+                <div id="principalHeaderConfigButtonDiv" onClick={handleCloseOrOpenOptions}>
+                    <button type='button'>
+                        <img src={config} alt="" />
+                    </button>
+                </div>
+
+                <button type="button" className='principalHeaderConfigOptions' onClick={logOut}>Logout</button>
             </Content>
         </Container>
     )
--- src/components/PrincipalHeader/styles.ts
+++ src/components/PrincipalHeader/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.header`
 
 interface ContentProps {
   hasProfilePhoto: boolean;
+  isOpenOptions: boolean;
 }
 
 export const Content = styled.div<ContentProps>`
@@ -16,7 +17,7 @@ export const Content = styled.div<ContentProps>`
 
   margin-left: 10%;
   margin-right: 10%;
-  padding-top: ${(props) => props.hasProfilePhoto ? '1.5rem' : '3rem'};
+  padding-top: ${(props) => (props.hasProfilePhoto ? "1.5rem" : "3rem")};
 
   div {
     color: #fff;
@@ -62,6 +63,56 @@ export const Content = styled.div<ContentProps>`
     &:hover {
       filter: brightness(0.9);
     }
+  }
+
+  #principalHeaderConfigButtonDiv {
+    position: absolute;
+    top: 0;
+    right: 0%;
+
+    display: flex;
+    flex-direction: column;
+
+    margin-right: 0;
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 3rem;
+      height: 3rem;
+
+      img {
+        width: 3.5rem;
+      }
+    }
+  }
+
+  .principalHeaderConfigOptions {
+    position: fixed;
+    top: 3rem;
+    right: -3rem;
+
+    background: var(--shape);
+    color: black;
+
+    padding: 0.5rem 1rem;
+    margin-right: 5rem;
+    border-radius: 0rem;
+    height: auto;
+
+    ${(props) => {
+      if (props.isOpenOptions) {
+        return css`
+          display: flex;
+        `;
+      } else {
+        return css`
+          display: none;
+        `;
+      }
+    }}
   }
 
   @keyframes appear {
